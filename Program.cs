7d4e65252d5bb095926534cb60b250249a1c5dcf
Дмитрий Балабанов <dmittrj@outlook.com>
2022-04-10 21:22:31 +0300

    Memory type menu

--- Program.cs
+++ Program.cs
@@ -4,9 +4,22 @@ namespace OS_Practice_6
 {
     class Program
     {
+        public static void OS_SelectType()
+        {
+            Console.WriteLine(" Выберите алгоритм управления памятью:");
+            Console.WriteLine("  1. Распределение памяти фиксированными разделами");
+            Console.WriteLine("  2. Распределение памяти перемещаемыми разделами");
+            Console.WriteLine("  3. Распределение памяти разделами переменной величины");
+            Console.WriteLine("  4. Страничное распределение памяти");
+            Console.WriteLine("  5. Страничное распределение памяти (упрощённый вариант)"); 
+            Console.WriteLine("  6. Сегментное распределение памяти");
+            Console.WriteLine("  7. Странично-сегметное распределение памяти");
+            Console.WriteLine("  8. Свопинг");
+            Console.Write(" >");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            OS_SelectType();
         }
     }
 }
