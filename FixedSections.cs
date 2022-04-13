29242d00ae95ff6b4d3118721ae3e6d373e83ace
Дмитрий Балабанов <dmittrj@outlook.com>
2022-04-13 19:28:14 +0300

    Section class

--- FixedSections.cs
+++ FixedSections.cs
@@ -8,5 +8,14 @@ namespace OS_Practice_6
 {
     class FixedSections
     {
+        public List<Section> Sections { get; set; }
+        public FixedSections(int count, int size, params int[] sections)
+        {
+            Sections = new List<Section>();
+            for (int i = 0; i < count; i++)
+            {
+                Sections.Add(new Section(size));
+            }
+        }
     }
 }
---
+++ Section.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OS_Practice_6
+{
+    class Section
+    {
+
+        public int Size { get; set; }
+        public int Free { get {
+                return 1;
+            } 
+        }
+
+        public Section(int size)
+        {
+            if (size <= 0) throw new Exception("Size can't be negative or zero");
+            Size = size;
+        }
+    }
+}
