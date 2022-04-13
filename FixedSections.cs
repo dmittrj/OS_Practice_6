8cb64b2b50dcc04bfdbf43bf95654f401ea08072
Дмитрий Балабанов <dmittrj@outlook.com>
2022-04-13 19:48:52 +0300

    Adding task

--- FixedSections.cs
+++ FixedSections.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,45 @@ namespace OS_Practice_6
     class FixedSections
     {
         public List<Section> Sections { get; set; }
-        public FixedSections(int count, int size, params int[] sections)
+        public FixedSections(int count, params int[] sections)
         {
+            int size = 64 * 1024 / count;
             Sections = new List<Section>();
             for (int i = 0; i < count; i++)
             {
-                Sections.Add(new Section(size));
+                try
+                {
+                    Sections.Add(new Section(size));
+                } catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                
+            }
+        }
+
+        public void AddProgram(FileInfo file)
+        {
+            if (file.Length > Sections[0].Size)
+            {
+                throw new Exception("Too big task");
+            }
+            int max = Sections[0].Free;
+            int max_number = 0;
+            for (int i = 0; i < Sections.Count; i++)
+            {
+                if (max > Sections[i].Free)
+                {
+                    max = Sections[i].Free;
+                    max_number = i;
+                }
+            }
+            if (max < file.Length)
+            {
+                throw new Exception("Too big task");
+            } else
+            {
+                Sections[max_number].AddTask(file);
             }
         }
     }
--- Section.cs
+++ Section.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,17 +9,34 @@ namespace OS_Practice_6
 {
     class Section
     {
-
+        /// <summary>
+        /// Task size, bytes
+        /// </summary>
         public int Size { get; set; }
+        /// <summary>
+        /// Free space, bytes
+        /// </summary>
         public int Free { get {
-                return 1;
+                long occupied = 0;
+                foreach (FileInfo task in Tasks)
+                {
+                    occupied += task.Length;
+                }
+                return (int)(Size - occupied);
             } 
         }
+        public List<FileInfo> Tasks { get; set; }
 
         public Section(int size)
         {
             if (size <= 0) throw new Exception("Size can't be negative or zero");
+            Tasks = new List<FileInfo>();
             Size = size;
+        }
+
+        public void AddTask(FileInfo task)
+        {
+            Tasks.Add(task);
         }
     }
 }
